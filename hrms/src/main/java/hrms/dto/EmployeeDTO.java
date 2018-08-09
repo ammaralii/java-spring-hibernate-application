2c72a145604cf5b7ae10c5d47c4f2d640c912780
Syed Ammar Ali <se.intern2@vantibolli.com>
2018-08-09 08:05:33 +0500

    Adding Employee DTO class

---
+++ hrms/src/main/java/hrms/dto/EmployeeDTO.java
@@ -0,0 +1,94 @@
+package hrms.dto;
+
+public class EmployeeDTO {
+    private Integer employeeId;
+    private String employeeName;
+    private String employeeEmail;
+    private String employeePhone;
+    private String employeeUsername;
+    private String employeePassword;
+    private String employeeBasicPay;
+    private String employeeRole;
+    private Integer companyId;
+    private Integer departmentId;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+
+    public String getEmployeePhone() {
+        return employeePhone;
+    }
+
+    public void setEmployeePhone(String employeePhone) {
+        this.employeePhone = employeePhone;
+    }
+
+    public String getEmployeeUsername() {
+        return employeeUsername;
+    }
+
+    public void setEmployeeUsername(String employeeUsername) {
+        this.employeeUsername = employeeUsername;
+    }
+
+    public String getEmployeePassword() {
+        return employeePassword;
+    }
+
+    public void setEmployeePassword(String employeePassword) {
+        this.employeePassword = employeePassword;
+    }
+
+    public String getEmployeeBasicPay() {
+        return employeeBasicPay;
+    }
+
+    public void setEmployeeBasicPay(String employeeBasicPay) {
+        this.employeeBasicPay = employeeBasicPay;
+    }
+
+    public String getEmployeeRole() {
+        return employeeRole;
+    }
+
+    public void setEmployeeRole(String employeeRole) {
+        this.employeeRole = employeeRole;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+}
