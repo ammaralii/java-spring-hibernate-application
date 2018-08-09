8febf7c2635071e0a731674606189ef55f90d1e6
Syed Ammar Ali <se.intern2@vantibolli.com>
2018-08-09 09:20:34 +0500

    Adding custom query of find last id from employee in employeeRepository

--- hrms/src/main/java/hrms/repository/EmployeeRepository.java
+++ hrms/src/main/java/hrms/repository/EmployeeRepository.java
@@ -1,7 +1,12 @@
 package hrms.repository;
 
 import hrms.domain.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends CrudRepository<Employee,Integer> {
+    @Query("SELECT e FROM Employee e order by e.employeeId desc")
+    public List<Employee> findLastId();
 }
