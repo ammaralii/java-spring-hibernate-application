21ed2b54bdc196a849131e2141c9e122ca287101
Syed Ammar Ali <se.intern2@vantibolli.com>
2018-08-10 16:23:14 +0500

    Change DocumentRepository extended class from crud to jpa

--- hrms/src/main/java/hrms/repository/DocumentsRepository.java
+++ hrms/src/main/java/hrms/repository/DocumentsRepository.java
@@ -1,7 +1,11 @@
 package hrms.repository;
 
 import hrms.domain.Documents;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DocumentsRepository extends CrudRepository<Documents,Long> {
+import java.util.List;
+
+public interface DocumentsRepository extends JpaRepository<Documents,Integer> {
 }
