78b8abfd3be4a44bf9cd6ae2f5b027e6adcee175
Syed Ammar Ali <se.intern2@vantibolli.com>
2018-08-13 15:17:06 +0500

    Changing extends respository from CrudRepository to PagingAndSortingRepository

--- hrms/src/main/java/hrms/repository/EmployeeRepository.java
+++ hrms/src/main/java/hrms/repository/EmployeeRepository.java
@@ -3,10 +3,12 @@ package hrms.repository;
 import hrms.domain.Employee;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
-public interface EmployeeRepository extends CrudRepository<Employee,Integer> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee,Integer> {
     @Query("SELECT e FROM Employee e order by e.employeeId desc")
     public List<Employee> findLastId();
 }
